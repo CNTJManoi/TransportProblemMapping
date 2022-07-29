c0e54b507ddcb9cc40e2e1988fdd821a2cd699ba
Алексей Коровченко <lecha3525@gmail.com>
2022-07-30 00:21:40 +0700

    Add unit test for northwest method

--- TransportAlgorithms.Tests/TransportTests.cs
+++ TransportAlgorithms.Tests/TransportTests.cs
@@ -31,5 +31,30 @@ namespace TransportAlgorithms.Tests
                 }
             }
         }
+        [TestMethod]
+        public void TestTransportNorthWest()
+        {
+            var transport = new TransportAlgorithms.TransportProblem();
+            var matr = new double[3, 5] {
+            { 20, 23, 20, 15, 24 },
+            { 29, 15, 16, 19, 29 },
+            { 6, 11, 10, 9, 8 },
+            };
+            var zapas = new int[3] { 320, 280, 250 };
+            var potbr = new int[5] { 150, 140, 110, 230, 220 };
+            var result = transport.FindSolution(matr, zapas, potbr, TypeAlgorithm.NorthWest);
+            var solut = new double[3, 5] { { 150, 140, 30, 0, 0},
+                {0,0,80,200,0 },
+                { 0,0,0,30,220} };
+            Assert.AreEqual(result.GetLength(0), solut.GetLength(0), "Не равны размеры массивов!");
+            Assert.AreEqual(result.GetLength(1), solut.GetLength(1), "Не равны размеры массивов!");
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.GetLength(1); j++)
+                {
+                    Assert.AreEqual(result[i, j], solut[i, j], 0.00000001);
+                }
+            }
+        }
     }
 }
